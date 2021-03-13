3ee2b258af909cc86c4800a38de9bd22f9dc7dde
Veronika <n.kanchura.dodo@gmail.com>
2021-03-13 21:12:41 +0400

    'Jv'

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[]args){
+        int sum = 1000;
+        int deposit = 1000;
+        int bonus = deposit / 100;
+        int total = sum + deposit + bonus;
+
+        System.out.println(bonus);
+        System.out.println(total);
+    }
+}
