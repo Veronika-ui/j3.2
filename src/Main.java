ee2c59b07d27c5c9c82bd1fbd478b0f38183ff4a
Veronika <n.kanchura.dodo@gmail.com>
2021-03-14 21:26:10 +0400

    'jv'

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 public class Main {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         int sum = 1000;
         int deposit = 1000;
-        int bonus = deposit / 100;
-        int total = sum + deposit + bonus;
+        if (deposit < 1000) {
+            int total = sum + deposit;
+            System.out.println(total);
+        } else {
+            int bonus = deposit / 100;
+            int total = sum + deposit + bonus;
+            System.out.println(bonus);
+            System.out.println(total);
+        }
 
-        System.out.println(bonus);
-        System.out.println(total);
+
+
     }
 }
