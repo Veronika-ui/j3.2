5ae7770a992664104b04e1a1cb404eff1a7345ef
Veronika <n.kanchura.dodo@gmail.com>
2021-03-14 22:09:03 +0400

    'j'

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,15 @@ public class Main {
     public static void main(String[] args) {
         int sum = 1000;
         int deposit = 1000;
+        int bonus;
+        int total;
         if (deposit < 1000) {
-            int total = sum + deposit;
-            System.out.println(total);
+            total = sum + deposit;
         } else {
-            int bonus = deposit / 100;
-            int total = sum + deposit + bonus;
-            System.out.println(bonus);
-            System.out.println(total);
+            bonus = deposit / 100;
+            total = sum + deposit + bonus;
         }
-
-
+        System.out.println(total);
 
     }
 }
